f2cd1c484943c017deccaba02f42a4fd9619fd4f
Awadhoot <awadhootk6@gmail.com>
2024-04-15 01:04:23 +0530

    try

---
+++ s3.js
@@ -0,0 +1,29 @@
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
+require('dotenv').config();
+
+const region = process.env.AWS_BUCKET_REGION
+const bucketName = process.env.AWS_BUCKET_NAME
+const accessKeyID = process.env.AWS_BUCKET_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_BUCKET_SECRET_ACCESS_KEY
+
+const s3 = new S3({
+    region,
+    accessKeyID,
+    secretAccessKey
+});
+
+function uploadFile(file) {
+    // file is multer object
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise();
+}
+
+exports.uploadFile = uploadFile;
